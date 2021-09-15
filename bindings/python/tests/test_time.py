61c3efc1fd2453e2c89bf5f506042e63f3f3de64
Alexandre Tuleu <alexandre.tuleu.2005@polytechnique.org>
2021-09-15 12:28:59 +0200

    Imports py_fort_myrmidon.Time unittest from `fort-time`

--- bindings/python/tests/test_time.py
+++ bindings/python/tests/test_time.py
@@ -47,3 +47,93 @@ class TimeTestCase(unittest.TestCase):
         t = m.Time(d)
         self.assertEqual(t, m.Time.Parse("2019-11-02T23:12:13.000014Z"))
         self.assertEqual(d, t.ToDateTime().astimezone(None))
+
+    def test_time_overflow(self):
+        with self.assertRaises(RuntimeError):
+            m.Time(2.0 ** 64)
+
+    def test_infinite_time_computation_overflow(self):
+        operations = [
+            (lambda: m.Time.Forever().Add(0), False),
+            (lambda: m.Time.Forever().Add(1), True),
+            (lambda: m.Time.Forever().Add(-1), True),
+            (lambda: m.Time.SinceEver().Add(0), False),
+            (lambda: m.Time.SinceEver().Add(1), True),
+            (lambda: m.Time.SinceEver().Add(-1), True),
+            (lambda: m.Time.SinceEver().Sub(m.Time()), True),
+            (lambda: m.Time.SinceEver().Sub(m.Time()), True),
+        ]
+        for op, hasError in operations:
+            if hasError:
+                with self.assertRaises(RuntimeError):
+                    op()
+            else:
+                op()
+
+    def test_time_infinite_rounding_is_noop(self):
+        self.assertTrue(m.Time.Forever().Round(0).Equals(m.Time().Forever()))
+        self.assertTrue(m.Time.SinceEver().Round(
+            0).Equals(m.Time().SinceEver()))
+
+    def test_time_rounding(self):
+        data = [
+            (m.Time.Parse("2020-03-20T15:34:08.865123567Z"),
+             m.Duration(10),
+             m.Time.Parse("2020-03-20T15:34:08.86512357Z")),
+            (m.Time.Parse("2020-03-20T15:34:08.865123567Z"),
+             m.Duration(100),
+             m.Time.Parse("2020-03-20T15:34:08.8651236Z")),
+            (m.Time.Parse("2020-03-20T15:34:08.865123567Z"),
+             1 * m.Duration.Microsecond,
+             m.Time.Parse("2020-03-20T15:34:08.865124Z")),
+            (m.Time.Parse("2020-03-20T15:34:08.865123567Z"),
+             10 * m.Duration.Microsecond,
+             m.Time.Parse("2020-03-20T15:34:08.86512Z")),
+            (m.Time.Parse("2020-03-20T15:34:08.865123567Z"),
+             100 * m.Duration.Microsecond,
+             m.Time.Parse("2020-03-20T15:34:08.8651Z")),
+            (m.Time.Parse("2020-03-20T15:34:08.865123567Z"),
+             1 * m.Duration.Millisecond,
+             m.Time.Parse("2020-03-20T15:34:08.865Z")),
+            (m.Time.Parse("2020-03-20T15:34:08.865123567Z"),
+             10 * m.Duration.Millisecond,
+             m.Time.Parse("2020-03-20T15:34:08.87Z")),
+            (m.Time.Parse("2020-03-20T15:34:08.865123567Z"),
+             100 * m.Duration.Millisecond,
+             m.Time.Parse("2020-03-20T15:34:08.9Z")),
+            (m.Time.Parse("2020-03-20T15:34:08.865123567Z"),
+             m.Duration.Second,
+             m.Time.Parse("2020-03-20T15:34:09Z")),
+            (m.Time.Parse("2020-03-20T15:34:08.865123567Z"),
+             2*m.Duration.Second,
+             m.Time.Parse("2020-03-20T15:34:08Z")),
+            (m.Time.Parse("2020-03-20T15:34:08.865123567Z"),
+             m.Duration.Minute,
+             m.Time.Parse("2020-03-20T15:34:00Z")),
+            (m.Time.Parse("2020-03-20T15:34:08.865123567Z"),
+             m.Duration.Hour,
+             m.Time.Parse("2020-03-20T16:00:00Z")),
+            (m.Time.Parse("2020-03-20T15:34:08.865123567Z"),
+             24 * m.Duration.Hour,
+             m.Time.Parse("2020-03-21T00:00:00Z")),
+        ]
+        for v, d, expected in data:
+            self.assertEqual(v.Round(d), expected)
+
+    def test_infinite_time_comparisons(self):
+        data = [
+            m.Time(),
+            m.Time().Add(2 ** 63 - 1),
+            m.Time().Add(-2 ** 63),
+        ]
+        for t in data:
+            self.assertTrue(t < m.Time.Forever())
+            self.assertTrue(t > m.Time.SinceEver())
+
+        self.assertFalse(m.Time.Forever() > m.Time.Forever())
+        self.assertFalse(m.Time.Forever() < m.Time.Forever())
+        self.assertTrue(m.Time.Forever() == m.Time.Forever())
+
+        self.assertFalse(m.Time.SinceEver() > m.Time.SinceEver())
+        self.assertFalse(m.Time.SinceEver() < m.Time.SinceEver())
+        self.assertTrue(m.Time.SinceEver() == m.Time.SinceEver())
