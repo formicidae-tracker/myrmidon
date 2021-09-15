c75db24f94d8f10bdbe25c7f69a12838383e0d1b
Alexandre Tuleu <alexandre.tuleu.2005@polytechnique.org>
2021-09-15 19:40:22 +0200

    Checks after a build

--- conda/install-r-fort-myrmidon.sh
+++ conda/install-r-fort-myrmidon.sh
@@ -5,7 +5,7 @@ pushd bindings/R/FortMyrmidon
 
 sed "s/Version: .*/Version: ${PKG_VERSION}/" ../DESCRIPTION.in > DESCRIPTION
 
-${R} CMD check .
 ${R} CMD INSTALL --preclean --build .
+${R} CMD check .
 
 popd
