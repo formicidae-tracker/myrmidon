fb04738eb13b2527e430b02c09c5ddc1f3690935
Alexandre Tuleu <alexandre.tuleu.2005@polytechnique.org>
2021-09-22 18:04:08 +0200

    Fixes r-fort-myrmidon conda recipe

--- conda/install-r-fort-myrmidon.sh
+++ conda/install-r-fort-myrmidon.sh
@@ -5,7 +5,9 @@ pushd bindings/R/FortMyrmidon
 
 sed "s/Version: .*/Version: ${PKG_VERSION}/" ../DESCRIPTION.in > DESCRIPTION
 
+export PKG_CONFIG_PATH=${PKG_CONFIG_PATH}:${PREFIX}/share/pkgconfig
+
 ${R} CMD INSTALL --preclean --build .
-${R} CMD check .
+${R} CMD check --no-manual .
 
 popd
