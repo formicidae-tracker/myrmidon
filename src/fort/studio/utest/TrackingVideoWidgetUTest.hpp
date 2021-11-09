3681fc5bafd80e43d2de3de0375a05f3d2bfb1a5
Alexandre Tuleu <alexandre.tuleu.2005@gmail.com>
2021-11-09 11:43:04 +0100

    Makes issues 211 unit tested

--- src/fort/studio/utest/TrackingVideoWidgetUTest.hpp
+++ src/fort/studio/utest/TrackingVideoWidgetUTest.hpp
@@ -6,6 +6,9 @@ class ExperimentBridge;
 class TrackingVideoWidget;
 
 #include <fort/studio/widget/TrackingVideoFrame.hpp>
+#include <fort/myrmidon/priv/Ant.hpp>
+
+
 
 class TrackingVideoWidgetUTest : public ::testing::Test {
 protected:
@@ -17,4 +20,6 @@ protected:
 	ExperimentBridge    * experiment;
 	TrackingVideoWidget * widget;
 	TrackingVideoFrame    frame;
+
+	fort::myrmidon::priv::Ant::Ptr ants[3];
 };
