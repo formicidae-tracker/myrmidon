fe8e103f8af7b2de4eac9a53d673f24665c69414
Alexandre Tuleu <alexandre.tuleu.2005@polytechnique.org>
2021-12-01 17:24:48 +0100

    Unit test Value equality operator

--- src/fort/myrmidon/types/ValueUTest.cpp
+++ src/fort/myrmidon/types/ValueUTest.cpp
@@ -64,5 +64,40 @@ TEST_F(ValueUTest,HasDefault) {
 }
 
 
+TEST_F(ValueUTest,HasEqualityOperator) {
+	std::vector<std::pair<Value,Value>> testdata
+		= {
+		   {true,false},
+		   {0,1},
+		   {0.0,1.0},
+		   {std::string("a"),std::string("b")},
+		   {Time(),Time().Add(1)},
+	};
+
+	for ( size_t i = 0 ; i < testdata.size() ; ++i ) {
+		std::ostringstream  oss;
+		oss << "testing {" << testdata[i].first << "," << testdata[i].second << "}";
+		SCOPED_TRACE(oss.str());
+		EXPECT_TRUE(testdata[i].first == testdata[i].first);
+		EXPECT_TRUE(testdata[i].second == testdata[i].second);
+		EXPECT_FALSE(testdata[i].first == testdata[i].second);
+		for ( size_t j = i+1; j < testdata.size(); ++j) {
+			std::ostringstream  oss;
+			oss << "against {" << testdata[j].first << "," << testdata[j].second << "}";
+			SCOPED_TRACE(oss.str());
+			EXPECT_FALSE(testdata[i].first == testdata[j].first);
+			EXPECT_FALSE(testdata[i].first == testdata[j].second);
+			EXPECT_FALSE(testdata[i].second == testdata[j].first);
+			EXPECT_FALSE(testdata[i].second == testdata[j].second);
+		}
+	}
+
+
+	EXPECT_TRUE(Value(true) == Value(true));
+	EXPECT_FALSE(Value(false) == Value(true));
+	EXPECT_FALSE(Value(false) == Value(0));
+}
+
+
 } // namespace myrmidon
 } // namespace fort
