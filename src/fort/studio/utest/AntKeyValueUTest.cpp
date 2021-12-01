fd55ff778be74c2ee100e64d89f0ec364034d93f
Alexandre Tuleu <alexandre.tuleu.2005@polytechnique.org>
2021-12-01 18:17:29 +0100

    Adds unit test for AntKeyValueBridge->dataModel()
    Very basic, but it seems ok.

--- src/fort/studio/utest/AntKeyValueUTest.cpp
+++ src/fort/studio/utest/AntKeyValueUTest.cpp
@@ -85,7 +85,7 @@ TEST_F(AntKeyValueUTest,TypeModel) {
 	          int(fmp::ValueType::TIME));
 }
 
-TEST_F(AntKeyValueUTest,ModificationTest) {
+TEST_F(AntKeyValueUTest,KeyModel) {
 	bridge->setExperiment(experiment);
 
 	QSignalSpy modified(bridge,SIGNAL(modified(bool)));
@@ -165,5 +165,110 @@ TEST_F(AntKeyValueUTest,ModificationTest) {
 	          "Time");
 	EXPECT_EQ(m->index(0,1).data(AntKeyValueBridge::KeyTypeRole).toInt(),
 	          quint32(fmp::ValueType::TIME));
+
+}
+
+void TestChild(const QAbstractItemModel * model,
+               const QModelIndex & parent,
+               const std::function<void (const QModelIndex & index, int row)> & test) {
+	for ( size_t i = 0; i < model->rowCount(parent); ++i ) {
+		auto index = model->index(i,0,parent);
+		SCOPED_TRACE(ToStdString(index.data(Qt::DisplayRole).toString())
+		             + " at "
+		             + std::to_string(i));
+		test(index,i);
+	}
+}
+
+
+TEST_F(AntKeyValueUTest,DataModel) {
+	auto m = bridge->dataModel();
+	EXPECT_EQ(m->rowCount(),0);
+
+	experiment->CreateAnt();
+	experiment->CreateAnt();
+	experiment->CreateAnt();
+
+	bridge->setExperiment(experiment);
+
+	EXPECT_EQ(m->rowCount(),3);
+	TestChild(m,QModelIndex(),
+	          []( const QModelIndex & index, int ) {
+		          EXPECT_EQ(index.model()->rowCount(index),0);
+	          });
+
+	bridge->setKey("isAlive",true);
+	bridge->setKey("behavior",std::string());
+
+	TestChild(m,QModelIndex(),
+	          []( const QModelIndex & antIndex, int ) {
+		          EXPECT_EQ(antIndex.model()->rowCount(antIndex),2);
+		          TestChild(antIndex.model(),antIndex,
+		                    [&] ( const QModelIndex & keyIndex, int row) {
+			                    EXPECT_EQ(keyIndex.model()->rowCount(keyIndex),0);
+			                    EXPECT_EQ(ToStdString(keyIndex.siblingAtColumn(3).data().toString()),
+			                              row == 0 ?  "default ()" : "default (true)" );
+		                    });
+	          });
+
+	bridge->setValue(1,"isAlive",fort::Time::SinceEver(),false);
+	auto ant1AliveIndex = m->index(1,0,m->index(0,0));
+	EXPECT_EQ(ToStdString(ant1AliveIndex.siblingAtColumn(3).data().toString()),
+	          "false");
+	EXPECT_EQ(m->rowCount(ant1AliveIndex),0);
+
+	m->setData(ant1AliveIndex.siblingAtColumn(3),"");
+	EXPECT_EQ(ToStdString(ant1AliveIndex.siblingAtColumn(3).data().toString()),
+	          "default (true)");
+	EXPECT_EQ(m->rowCount(ant1AliveIndex),0);
+
+	bridge->setValue(1,"isAlive",fort::Time(),false);
+	EXPECT_EQ(ToStdString(ant1AliveIndex.siblingAtColumn(3).data().toString()),
+	          "default (true)");
+	EXPECT_EQ(m->rowCount(ant1AliveIndex),1);
+	auto timedIndex = m->index(0,0,ant1AliveIndex);
+	EXPECT_EQ(ToStdString(timedIndex.siblingAtColumn(3).data().toString()),
+	          "false");
+	EXPECT_EQ(ToStdString(timedIndex.siblingAtColumn(2).data().toString()),
+	          fort::Time().Format());
+
+
+	bridge->setValue(1,"isAlive",fort::Time().Add(-1),true);
+	EXPECT_EQ(m->rowCount(ant1AliveIndex),2);
+	auto timed1Index = m->index(0,0,ant1AliveIndex);
+	EXPECT_EQ(ToStdString(timed1Index.siblingAtColumn(3).data().toString()),
+	          "true");
+	EXPECT_EQ(ToStdString(timed1Index.siblingAtColumn(2).data().toString()),
+	          fort::Time().Add(-1).Format());
+
+	auto timed2Index = m->index(1,0,ant1AliveIndex);
+	EXPECT_EQ(ToStdString(timed2Index.siblingAtColumn(3).data().toString()),
+	          "false");
+	EXPECT_EQ(ToStdString(timed2Index.siblingAtColumn(2).data().toString()),
+	          fort::Time().Format());
+
+	m->setData(timed2Index.siblingAtColumn(2),
+	           fort::Time().Add(-2).Format().c_str());
+	timed1Index = m->index(0,0,ant1AliveIndex);
+	EXPECT_EQ(ToStdString(timed1Index.siblingAtColumn(3).data().toString()),
+	          "false");
+	EXPECT_EQ(ToStdString(timed1Index.siblingAtColumn(2).data().toString()),
+	          fort::Time().Add(-2).Format());
+	timed2Index = m->index(1,0,ant1AliveIndex);
+	EXPECT_EQ(ToStdString(timed2Index.siblingAtColumn(3).data().toString()),
+	          "true");
+	EXPECT_EQ(ToStdString(timed2Index.siblingAtColumn(2).data().toString()),
+	          fort::Time().Add(-1).Format());
+
+	bridge->deleteValue(1,"isAlive",fort::Time().Add(-1));
+	EXPECT_EQ(m->rowCount(ant1AliveIndex),1);
+	timedIndex = m->index(0,0,ant1AliveIndex);
+	EXPECT_EQ(ToStdString(timedIndex.siblingAtColumn(3).data().toString()),
+	          "false");
+	EXPECT_EQ(ToStdString(timedIndex.siblingAtColumn(2).data().toString()),
+	          fort::Time().Add(-2).Format());
+
+
+
 
 }
--- src/fort/studio/utest/AntMetadataUTest.hpp
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-
-#include <fort/myrmidon/priv/Experiment.hpp>
