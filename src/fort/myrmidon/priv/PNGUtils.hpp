1be6c7f68ae416e174741287b65e54385256af4b
Alexandre Tuleu <alexandre.tuleu.2005@polytechnique.org>
2023-12-08 14:50:02 +0100

    [WIP]

--- src/fort/myrmidon/priv/PNGUtils.hpp
+++ src/fort/myrmidon/priv/PNGUtils.hpp
@@ -2,12 +2,19 @@
 
 #include <apriltag/common/image_u8.h>
 #include <filesystem>
+#include <memory>
 
 namespace fort {
 namespace myrmidon {
 namespace priv {
 
-image_u8_t ReadPNG(const std::filesystem::path &path);
+// Using a stateless lambda to have unique pointer the same size than raw
+// pointers.
+const auto deleteImageU8 = [](image_u8_t *img) { image_u8_destroy(img); };
+
+using ImageU8Ptr = std::unique_ptr<image_u8_t, decltype(deleteImageU8)>;
+
+ImageU8Ptr ReadPNG(const std::filesystem::path &path);
 
 void WritePNG(const std::filesystem::path &path, const image_u8_t &image);
 
