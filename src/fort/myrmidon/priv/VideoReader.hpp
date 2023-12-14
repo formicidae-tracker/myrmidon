79343f23e1e0788829a9a86554ac5ff332f0ab8f
Alexandre Tuleu <alexandre.tuleu.2005@polytechnique.org>
2023-12-14 18:26:15 +0100

    [WIP] decode and scale / convert with libav.
    Still need testing and seeking.

--- src/fort/myrmidon/priv/VideoReader.hpp
+++ src/fort/myrmidon/priv/VideoReader.hpp
@@ -1,21 +1,41 @@
 #pragma once
 
+#include "fort/myrmidon/priv/PNGUtils.hpp"
 #include <fort/time/Time.hpp>
 
 #include <filesystem>
+#include <functional>
 #include <memory>
 #include <tuple>
 
+extern "C" {
 struct image_u8;
 typedef struct image_u8 image_u8_t;
+}
 
 namespace fort {
 namespace myrmidon {
 namespace priv {
 
+class VideoFrame {
+public:
+	struct ImageData;
+	using Ptr = std::unique_ptr<VideoFrame>;
+
+	image_u8_t AsImageU8() const noexcept;
+
+private:
+	friend class VideoReader;
+
+	std::unique_ptr<ImageData, std::function<void(ImageData *)>> d_imageData;
+};
+
 class VideoReader {
 public:
-	VideoReader(const std::filesystem::path &path);
+	VideoReader(
+	    const std::filesystem::path &path,
+	    std::tuple<int, int>         targetSize = {-1, -1}
+	);
 
 	~VideoReader();
 
@@ -27,14 +47,15 @@ public:
 
 	size_t Position() const noexcept;
 
-	void Grab(const image_u8_t &image);
-
 	void Seek(size_t position);
 
 	void Seek(fort::Duration duration);
 
+	VideoFrame::Ptr Grab();
+
 private:
 	struct Implementation;
+
 	std::unique_ptr<Implementation> d_implementation;
 };
 
