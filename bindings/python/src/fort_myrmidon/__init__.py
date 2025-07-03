86d811059c66a6f7efa5896646a4663639c746fc
Alexandre Tuleu <alexandre.tuleu.2005@polytechnique.org>
2025-07-03 14:18:10 +0200

    Allows for different python .so module name

--- bindings/python/src/fort_myrmidon/__init__.py
+++ bindings/python/src/fort_myrmidon/__init__.py
@@ -1,3 +1,3 @@
 from __future__ import annotations
 
-from .fort_myrmidon import *
+from ._fort_myrmidon import *
--- bindings/python/src/main.cpp
+++ bindings/python/src/main.cpp
@@ -3,13 +3,17 @@
 #ifndef VERSION_INFO
 #include <fort/myrmidon/myrmidon-version.h>
 #else
-#define STRINGIFY(x) #x
+#define STRINGIFY(x)       #x
 #define MACRO_STRINGIFY(x) STRINGIFY(x)
+#endif
+
+#ifndef FM_PYTHON_PACKAGE_NAME
+#error "Must define FM_PYTHON_PACKAGE_NAME"
 #endif
 
 namespace py = pybind11;
 
-PYBIND11_MODULE(fort_myrmidon, m) {
+PYBIND11_MODULE(FM_PYTHON_PACKAGE_NAME, m) {
 	m.doc() = "Bindings for libfort-myrmidon"; // optional module docstring
 
 	BindTypes(m);
