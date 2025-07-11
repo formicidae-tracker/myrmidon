377b5f17ae4768f9d487ed8243e5943c2dd16ec2
Alexandre Tuleu <alexandre.tuleu.2005@polytechnique.org>
2025-07-11 15:48:15 +0200

    Allows for functor style query from Python
    It should allow us to reduce the memory pressure.

--- bindings/python/src/BindTypes.hpp
+++ bindings/python/src/BindTypes.hpp
@@ -1,21 +1,22 @@
 #pragma once
 
-#include <pybind11/pybind11.h>
+#include <pybind11/chrono.h>
 #include <pybind11/eigen.h>
+#include <pybind11/functional.h>
+#include <pybind11/operators.h>
+#include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
 #include <pybind11/stl_bind.h>
-#include <pybind11/operators.h>
-#include <pybind11/chrono.h>
 
-#include <fort/myrmidon/types/Typedefs.hpp>
 #include <fort/myrmidon/Shapes.hpp>
+#include <fort/myrmidon/types/Typedefs.hpp>
 
 namespace fort {
 namespace myrmidon {
 struct VideoFrameData;
 struct VideoSegment;
-}
-}
+} // namespace myrmidon
+} // namespace fort
 
 PYBIND11_MAKE_OPAQUE(fort::myrmidon::Vector2dList)
 PYBIND11_MAKE_OPAQUE(fort::myrmidon::Shape::List)
