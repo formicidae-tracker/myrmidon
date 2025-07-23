9b5d48e47b166a29735c57da642ba248ab937b38
Alexandre Tuleu <alexandre.tuleu.2005@polytechnique.org>
2025-07-23 17:24:54 +0200

    Allows for timestamp to start at 0

--- src/fort/myrmidon/priv/TimeUtils.hpp
+++ src/fort/myrmidon/priv/TimeUtils.hpp
@@ -8,14 +8,18 @@ namespace fort {
 namespace myrmidon {
 namespace priv {
 
-inline static
-Time TimeFromFrameReadout(const fort::hermes::FrameReadout & ro, Time::MonoclockID monoID) {
-	if (ro.error() !=  fort::hermes::FrameReadout_Error_NO_ERROR || ro.timestamp() == 0) {
+inline static Time TimeFromFrameReadout(
+    const fort::hermes::FrameReadout &ro, Time::MonoclockID monoID
+) {
+	if (ro.error() != fort::hermes::FrameReadout_Error_NO_ERROR) {
 		return Time::FromTimestamp(ro.time());
 	}
-	return Time::FromTimestampAndMonotonic(ro.time(),ro.timestamp() * 1000, monoID);
+	return Time::FromTimestampAndMonotonic(
+	    ro.time(),
+	    ro.timestamp() * 1000,
+	    monoID
+	);
 }
-
 
 } //namespace priv
 } //namespace myrmidon
